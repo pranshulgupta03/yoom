75cf2c82780f6a30778fd32589cd0b4d88c36675
pranshulgupta03 <guptapranshu0801@gmail.com>
2024-05-14 10:01:08 +0530

    fixing error

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -14,6 +14,7 @@ import ReactDatePicker from 'react-datepicker';
 import { useToast } from './ui/use-toast';
 import { Input } from './ui/input';
 
+
 const initialValues = {
   dateTime: new Date(),
   description: '',
